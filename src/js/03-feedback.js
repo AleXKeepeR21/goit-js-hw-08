0f5f7d3ba498f7816d9ab5c94598529c411bc6e6
AleXKeepeR21 <106617654+AleXKeepeR21@users.noreply.github.com>
2022-10-29 11:36:06 +0300

    bagfix on 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,22 +5,19 @@ const formData = {};
 
 let getEl = selector => document.querySelector(selector);
 getEl('.feedback-form').addEventListener('submit', onFormSubmit);
-getEl('.feedback-form textarea').addEventListener(
-  'input',
-  throttle(onTextareaInput, 500)
-);
 
-getEl('.feedback-form').addEventListener('input', event => {
-  formData[event.target.name] = event.target.value;
-  //   console.log(formData);
-
-  const dataOfKey = JSON.stringify(formData);
-  localStorage.setItem(STORAGE_KEY, dataOfKey);
-});
+getEl('.feedback-form').addEventListener('input', throttle(onFormInput, 500));
 
 getEl('.feedback-form input');
 
 populateTextarea();
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+
+  const dataOfKey = JSON.stringify(formData);
+  localStorage.setItem(STORAGE_KEY, dataOfKey);
+}
 
 function onFormSubmit(event) {
   event.preventDefault();
@@ -28,22 +25,15 @@ function onFormSubmit(event) {
   localStorage.removeItem(STORAGE_KEY);
 }
 
-function onTextareaInput(event) {
-  const message = event.target.value;
-  localStorage.setItem(STORAGE_KEY, message);
-}
-
 function populateTextarea() {
   const savedMessage = localStorage.getItem(STORAGE_KEY);
   const parsedMessage = JSON.parse(savedMessage);
   if (savedMessage) {
-    if (parsedMessage.email) {
-      getEl('.feedback-form input').value = parsedMessage.email;
-      formData[getEl('.feedback-form input').name] = parsedMessage.email;
-    }
-    if (parsedMessage.message) {
-      getEl('.feedback-form textarea').value = parsedMessage.message;
-      formData[getEl('.feedback-form textarea').name] = parsedMessage.message;
-    }
+    getEl('.feedback-form input').value = parsedMessage.email || '';
+    formData[getEl('.feedback-form input').name] = parsedMessage.email || '';
+
+    getEl('.feedback-form textarea').value = parsedMessage.message || '';
+    formData[getEl('.feedback-form textarea').name] =
+      parsedMessage.message || '';
   }
 }
